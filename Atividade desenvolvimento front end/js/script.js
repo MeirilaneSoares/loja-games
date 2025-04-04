3ef7d57ca2cdd39cb0618fdfbdea4e5978c6521d
meirilanesoares <meirilanesoares26@gmail.com>
2025-04-04 16:51:53 -0300

    criação das funções do javascript

--- Atividade desenvolvimento front end/js/script.js
+++ Atividade desenvolvimento front end/js/script.js
@@ -1,27 +1,34 @@
 //efeito do botão voltar ao Topo
-
-function Topo(){
+ 
+function topo(){
     window.scrollTo(
         {
             top:0,
             left:0,
-            behavior:'smooth'
-
+            behavior:"smooth"
         }
-    )
-
+    );
 }
+ 
 //Validação de Login
-
 function login(){
-    var logado = 0;
-    var usuario = document.getElementById("usuario").Value;
-    var senha = document.getElementById("senha").Value;
+    let logado = 0;
+    let usuario = document.getElementById('usuario').value;
+    let senha = document.getElementById('senha').value;
+ 
     if(usuario == 'admin' && senha == '123456'){
-        window.location = "index.html";
-        logado = 1; 
+        window.location = 'index.html';
+        logado = 1;
     }
-
+ 
+    if(logado == 0 ){
+        alert("Acesso negado. Dados incorretos");
+    }
+ 
 }
-
 //Ativar alert no botão cadastrar
+function cadastro(){
+    alert("Cadastrado com sucesso!");
+ 
+    window.location.href = "index.html"
+}
